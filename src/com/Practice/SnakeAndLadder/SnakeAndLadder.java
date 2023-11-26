0cc95246af48e893fdccac2e0c14ea4ffbac3686
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-11-26 17:08:07 +0530

    Added :UC1 task

---
+++ src/com/Practice/SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package com.Practice.SnakeAndLadder;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and ladder..");
+        final int START_POSITION=0;
+        int player=0;
+    }
+}
